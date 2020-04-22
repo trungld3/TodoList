ddc7a370aa076734c7f5fdf9b79a817d9e24fcf4
trungld3 <k21tpm4@gmail.com>
2020-04-22 11:34:37 +0700

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by TrungLD on 4/22/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

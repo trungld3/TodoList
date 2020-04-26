04e005cfc8b9da51c68e0a256ec1cc7cbe46d8bf
trungld3 <k21tpm4@gmail.com>
2020-04-26 12:07:32 +0700

    fix loi patch

---
+++ TodoList/Data Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  TodoList
+//
+//  Created by TrungLD on 4/26/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+class Data: Object {
+   @objc dynamic var name: String = "trung"
+   @objc dynamic var age : Int = 21
+}
---
+++ TodoList/Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+use_modular_headers!
+platform :ios, '9.0'
+
+target 'TodoList' do
+
+
+
+    pod 'RealmSwift'
+end
